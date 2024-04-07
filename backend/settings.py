b6bf8d0348216c7c489a0119295c2dee79d64804
Antony dos santos felisberto <88542984+AntonyFelisberto@users.noreply.github.com>
2024-04-07 15:42:19 -0300

    atualizando app

--- backend/settings.py
+++ backend/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = 'django-insecure-$2be8f0e78%s6t2ybq7(51&9kvzhp2(fk@&6@%e@cgtqvszw&t
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["django-shopping-1.onrender.com"]
+ALLOWED_HOSTS = ["django-shopping-1.onrender.com","localhost:10000","localhost:10012"]
 
 
 # Application definition
@@ -100,7 +100,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS':  [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -143,10 +143,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+STATIC_URL = "static/"
 MEDIA_URL =  'images/'
-
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
     BASE_DIR / "static"
--- backend/urls.py
+++ backend/urls.py
@@ -17,13 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
-from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/products/',include('base.urls.product_urls')),
     path('api/users/',include('base.urls.user_urls')),
     path('api/orders/',include('base.urls.order_urls')),
-] + static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+] + staticfiles_urlpatterns()
 
-urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
